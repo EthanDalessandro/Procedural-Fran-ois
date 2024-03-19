3a7d80cd6508c57086828a6aae6e2b800fb78221
D'alessandro Ethan <roger.paldi@yahoo.com>
2024-03-19 09:43:56 +0100

    Main Menu Added

--- Assets/Scripts/HUD/ShowCloseHUD.cs
+++ Assets/Scripts/HUD/ShowCloseHUD.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class ShowCloseHUD : MonoBehaviour
 {
-    public GameObject ObjectToPlayWith;
-
-    public void CloseOpenObject()
+    public void CloseOpenObject(GameObject objectToPlayWith)
     {
-        ObjectToPlayWith.SetActive(!ObjectToPlayWith.activeSelf);
+        objectToPlayWith.SetActive(!objectToPlayWith.activeSelf);
     }
 }
